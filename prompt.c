45dce5d3c4693d7cda70bcd700fde2d5ad1f79d3
kevanlucc <kevanlucchini@gmail.com>
2018-03-29 03:53:44 +0000

    add error handling for failure to print prompt

--- prompt.c
+++ prompt.c
@@ -5,5 +5,9 @@ void prompt()
         char *show_prompt = "bkash$ ";
         int len = strlen(show_prompt);
 
-        write(STDOUT_FILENO, show_prompt, len);
+        if (write(STDOUT_FILENO, show_prompt, len) == -1)
+	{
+		perror("prompt");
+		return (NULL);
+	}
 }
