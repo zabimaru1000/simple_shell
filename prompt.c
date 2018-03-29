4eb210c29adc653e827f347ceb9e10fc8db63fcc
kevanlucc <kevan.lucchini@gmail.com>
2018-03-29 04:39:39 +0000

    prompt file betty compliant

--- prompt.c
+++ prompt.c
@@ -1,11 +1,14 @@
 #include "shell.h"
-
-void prompt()
+/**
+ * prompt - prints the shell prompt
+ * Description: name of shell is the authors initials
+ */
+void prompt(void)
 {
-        char *show_prompt = "bkash$ ";
-        int len = strlen(show_prompt);
+	char *show_prompt = "bkash$ ";
+	int len = strlen(show_prompt);
 
-        if (write(STDOUT_FILENO, show_prompt, len) == -1)
+	if (write(STDOUT_FILENO, show_prompt, len) == -1)
 	{
 		perror("prompt");
 		return (NULL);
